e06273388ed93e58b361df359cfb2ed2123d25b5
Mykola Makhin <mvmn.inbox@gmail.com>
2026-01-11 19:43:09 +0200

    Use docker instead of Podman

--- xquartz/run.sh
+++ xquartz/run.sh
@@ -1,7 +1,9 @@
 export PATH="/opt/X11/bin:$PATH"
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-podman run --pull=never --rm -it --name telegram \
-       -e DISPLAY=$(xhost + $(hostname) > /dev/null; echo $IP):0 \
-       -v /etc/localtime:/etc/localtime:ro \
-       -v ~/.TelegramDesktop:/home/user/.local/share/TelegramDesktop/ "library/telegram-desktop:latest"
 
+docker run --pull=never --rm -it --name telegram \
+       --hostname=$(hostname) \
+	       -e DISPLAY=$(xhost + $(hostname) > /dev/null; echo $IP):0 \
+	       -v /etc/localtime:/etc/localtime:ro \
+       -e PULSE_SERVER=unix:$XDG_RUNTIME_DIR/pulse/native \
+       -v ~/.TelegramDesktop:/home/user/.local/share/TelegramDesktop/ "library/telegram-desktop:latest"
